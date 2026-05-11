5a96f2fd6c837462e20dc83b698ac93fd2c321e6
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-11 22:08:27 +0300

    initalize my project

---
+++ bags-store/src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Loader from "./Loader";
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+  if (loading) return <Loader />;
+  if (!isAuthenticated) return <Navigate to="/admin/login" replace />;
+  return children;
+};
+
+export default ProtectedRoute;
