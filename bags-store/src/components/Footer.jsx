d35368392d42b65f3fc126741dfa98fa72ba089c
Fredrick Mwangi <fredmallin49@gmail.com>
2026-05-17 11:17:10 +0300

    made deletions on footer.jsx

--- bags-store/src/components/Footer.jsx
+++ bags-store/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-bottom">
         <p>
-          {/* HIDDEN ADMIN TRIGGER
+          {/* HIDDEN ADMIN
           <span
             onClick={() => navigate("/admin/login")}
             style={{
